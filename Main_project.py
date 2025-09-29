c5cbf0aea575a91f4a93e450b9c593928ffedac4
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-29 21:16:08 +0530

    file handling

--- Main_project.py
+++ Main_project.py
@@ -7,7 +7,10 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 import os
 data_set=(r'D:\Python project\Main_project\archive(2)')
-genres=(r'D:\Python project\Main_project\archive (2)\genres\hiphop').split()
+genres=(r'D:\Python project\Main_project\archive (2)\genres').split()
 X,y=[],[]
-for gener in genres:
-    
+for genere in genres:
+    folder=os.path.join(data_set,genere)
+    for filename in os.listdir(folder):
+        file_path=os.path.join(folder,filename)
+        
