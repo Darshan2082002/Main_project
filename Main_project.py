b5857ef010a9b1376fa1d17bf514e0e43593fd60
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-29 20:18:10 +0530

    Adding the signal processing file to the Main_project

--- Main_project.py
+++ Main_project.py
@@ -1,3 +1,7 @@
 import numpy as np #To deal with array
 import pandas as pd # To deal with file 
 import matplotlib.pyplot as plt # Plot the graph 
+import librosa #for aduio Processing 
+import librosa.display
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
