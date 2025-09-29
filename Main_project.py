8943e276b830aef239e48e4b9679eacc36652882
Darshan2082002 <darshanbalu20@gmail.com>
2025-09-29 21:05:26 +0530

    Working on folder path

--- Main_project.py
+++ Main_project.py
@@ -6,3 +6,8 @@ import librosa.display
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 import os
+data_set=(r'D:\Python project\Main_project\archive(2)')
+genres=(r'D:\Python project\Main_project\archive (2)\genres\hiphop').split()
+X,y=[],[]
+for gener in genres:
+    
